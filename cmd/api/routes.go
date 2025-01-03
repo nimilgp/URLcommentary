98f7f9109365e8e4681389d3c4cd1f18bae8aa03
nimil <nimilgeorgepradeepngp@gmail.com>
2025-01-03 11:50:54 +0530

    seperated out json writer helper

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,12 +10,12 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/healthcheck", app.getHealthcheck)
-	router.HandlerFunc(http.MethodGet, "/view/newest/comments/:start/:stop", app.getNewestComments)
-	router.HandlerFunc(http.MethodGet, "/view/oldest/comments/:start/:stop", app.getOldestComments)
-	router.HandlerFunc(http.MethodGet, "/view/relevant/comments/:start/:stop", app.getRelevantComments)
-	router.HandlerFunc(http.MethodPost, "/create/comment", app.postComment)
-	router.HandlerFunc(http.MethodGet, "/view/reply/comments/", app.getReplyComments)
-	router.HandlerFunc(http.MethodPost, "/create/reply/comment", app.postReplyComment)
+	router.HandlerFunc(http.MethodGet, "/view/newest/comments/:forurl/:start/", app.getNewestComments)
+	router.HandlerFunc(http.MethodGet, "/view/oldest/comments/:forurl/:start/", app.getOldestComments)
+	router.HandlerFunc(http.MethodGet, "/view/relevant/comments/:forurl/:start/", app.getRelevantComments)
+	router.HandlerFunc(http.MethodPost, "/create/comment/:forurl", app.postComment)
+	router.HandlerFunc(http.MethodGet, "/view/reply/comments/:forurl/:commentid/:start/", app.getReplyComments)
+	router.HandlerFunc(http.MethodPost, "/create/reply/comment/:forurl/:commentid", app.postReplyComment)
 
 	return router
 }
