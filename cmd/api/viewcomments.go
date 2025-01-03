33b153ba4fae5f154703069142e7b384c3597fbf
nimilgp <nimilgeorgepradeepngp@gmail.com>
2025-01-03 20:04:29 +0530

    dummy data view endpoints

--- cmd/api/viewcomments.go
+++ cmd/api/viewcomments.go
@@ -6,7 +6,33 @@ import (
 )
 
 func (app *application) getNewestComments(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "getNewestComments")
+	js := `{
+		"new_comments": [
+			{
+				"user_id": "mathew",
+				"comment_content": "In another life, I would have really liked just doing laundry and taxes with you." 
+			},
+			{
+				"user_id" : "mark",
+				"comment_content": "I am Iron Man." 
+			},
+			{
+				"user_id": "luke",
+				"comment_content": "You are a sad, strange, little man, and you have my pity. Farewell." 
+			},
+			{
+				"user_id": "john",
+				"comment_content":"Never gonna give you up, Never gonna let you down"
+			},
+			{
+				"user_id": "doe",
+				"comment_content": "hello how do you do?" 
+			}
+		]
+	}`
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
 }
 
 func (app *application) getOldestComments(w http.ResponseWriter, r *http.Request) {
@@ -18,5 +44,19 @@ func (app *application) getRelevantComments(w http.ResponseWriter, r *http.Reque
 }
 
 func (app *application) getReplyComments(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "getReplyComments")
+	js := `{
+		"new_comments": [
+			{
+				"user_id": "john",
+				"comment_content":"Never gonna give you up, Never gonna let you down"
+			},
+			{
+				"user_id": "doe",
+				"comment_content": "hello how do you do?" 
+			}
+		]
+	}`
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
 }
