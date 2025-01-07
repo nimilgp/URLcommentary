14bc36ead0ef792d8e37e9a87e7ab355cbdc2a95
nimil <nimilgeorgepradeepngp@gmail.com>
2025-01-07 13:38:10 +0530

    helper to read id

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
@@ -21,4 +25,13 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data any, h
 	w.WriteHeader(status)
 	w.Write(js)
 	return nil
+}
+
+func (app *application) readIDParam(r *http.Request, paramName string) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(params.ByName(paramName), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+	return id, nil
 }
