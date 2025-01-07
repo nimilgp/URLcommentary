4ccf3a69809dddad48112e201d6158c13e658261
nimil <nimilgeorgepradeepngp@gmail.com>
2025-01-07 13:28:07 +0530

    forurl passed as query param

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,12 +10,12 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/healthcheck", app.getHealthcheck)
-	router.HandlerFunc(http.MethodGet, "/view/newest/comments/:forurl/:start/", app.getNewestComments)
-	router.HandlerFunc(http.MethodGet, "/view/oldest/comments/:forurl/:start/", app.getOldestComments)
-	router.HandlerFunc(http.MethodGet, "/view/relevant/comments/:forurl/:start/", app.getRelevantComments)
-	router.HandlerFunc(http.MethodPost, "/create/comment/:forurl", app.postComment)
-	router.HandlerFunc(http.MethodGet, "/view/reply/comments/:forurl/:commentid/:start/", app.getReplyComments)
-	router.HandlerFunc(http.MethodPost, "/create/reply/comment/:forurl/:commentid", app.postReplyComment)
+	router.HandlerFunc(http.MethodGet, "/view/newest/comments/:start", app.getNewestComments)
+	router.HandlerFunc(http.MethodGet, "/view/oldest/comments/:start", app.getOldestComments)
+	// router.HandlerFunc(http.MethodGet, "/view/relevant/comments/:start", app.getRelevantComments)
+	router.HandlerFunc(http.MethodPost, "/create/comment", app.postComment)
+	router.HandlerFunc(http.MethodGet, "/view/reply/comments/:commentid/:start", app.getReplyComments)
+	router.HandlerFunc(http.MethodPost, "/create/reply/comment/:commentid", app.postReplyComment)
 
 	return app.enableCORS(router)
 }
