d53b3a17a0610e0b168122de48c02e8dc3cdd18b
nimil <nimilgeorgepradeepngp@gmail.com>
2025-03-23 01:01:17 +0530

    trigger to handle comments count

--- sqlc/triggers.sql
+++ sqlc/triggers.sql
@@ -27,3 +27,35 @@ CREATE TRIGGER child_comments_insert_trigger
 BEFORE INSERT ON ChildComments
 FOR EACH ROW
 EXECUTE FUNCTION set_commentid_childcomments();
+
+--  increment Page's comment count on insert in ParentComments
+CREATE OR REPLACE FUNCTION increment_page_comments_count()
+RETURNS TRIGGER AS $$
+BEGIN
+  UPDATE Pages
+  SET CommentsCount = CommentsCount + 1
+  WHERE PageId = NEW.PageId;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER increment_page_comments_count_trigger
+AFTER INSERT ON ParentComments
+FOR EACH ROW
+EXECUTE FUNCTION increment_page_comments_count();
+
+--  increment ParentComment's child comment count on insert in ChildComments
+CREATE OR REPLACE FUNCTION increment_parent_comment_child_count()
+RETURNS TRIGGER AS $$
+BEGIN
+  UPDATE ParentComments
+  SET ChildCommentCount = ChildCommentCount + 1
+  WHERE PageId = NEW.PageId AND CommentId = NEW.ParentCommentId;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER increment_parent_comment_child_count_trigger
+AFTER INSERT ON ChildComments
+FOR EACH ROW
+EXECUTE FUNCTION increment_parent_comment_child_count();
